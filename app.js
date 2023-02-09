3058aab5c96134651b9dfb175def5eef027178ff
USMAN SIDDIQUI <121655186+Usmansiddiquu@users.noreply.github.com>
2023-02-09 19:35:59 +0500

    app.js

---
+++ app.js
@@ -0,0 +1,186 @@
+// var cityname = prompt("enter city name")
+// if(cityname==="karachi"){
+//     alert("Welcome to city of lights")
+// }else{
+//     alert("please input correct city")
+// }
+
+// var gender = prompt("enter your gender")
+// if(gender==="male"){
+//     alert("Good Morning Sir.")
+// }else if(gender==="female"){
+//     alert("Good Morning Ma’am.")
+// }
+
+// var trafficsignal = prompt("Enter Signal color")
+// if(trafficsignal==="red"){
+// alert("Must Stop")
+// }
+// if(trafficsignal==="yellow"){
+//     alert("Ready to Move")
+// }
+// if(trafficsignal==="green"){
+//         alert("Move Now")
+// }
+
+// var fuel = prompt("input remaining fuel in car")
+// if(fuel<"0.25"){
+//     alert("Please refill the fuel in your car")
+// }
+
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+// if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+// if("car" < "cat"){
+// alert("car is smaller than cat");
+// }
+
+// var subject = prompt("Enter subject number")
+// if(subject>="80"){
+// alert("A-one");
+// alert("Excellent")
+// }
+// var subject = prompt("Enter subject number")
+// if(subject>="70"){
+// alert("A");
+// alert("Good")
+// }
+// var subject = prompt("Enter subject number")
+// if(subject>="60"){
+// alert("B");
+// alert("you need to improve")
+// }
+// var subject = prompt("Enter subject number")
+// if(subject<"60"){
+// alert("Fail");
+// alert("Sorry")
+// }
+// document.write("<h1>Marks sheet</h1><h5>Total Marks=300</h5><h5>Marks Obtained=219</h5><h5>Percentage=73%</h5><h5>Grade=B</h5><h5>Remarks=You need to improve</h5>")
+
+// q7
+
+// var secretnumber= prompt("Enter secret number")
+// if(secretnumber==="3"){
+//     alert(" Bingo! Correct answer.")
+// }
+// else{
+//     secretnumber===1
+//     alert("“Close enough to the correct answer”.")
+// }
+
+// q8
+
+// let num = prompt("Enter a number: ");
+
+// if (num % 3 === 0) {
+//   alert(num + " is divisible by 3.");
+// } else {
+//   alert(num + " is not divisible by 3.");
+// }
+
+// q9
+
+// var num = prompt("Enter a number: ");
+
+// if (num % 2 === 0) {
+//   alert(num + " Is a even number");
+// } else {
+//   alert(num + " Is a odd number.");
+// }
+
+// q10
+
+// var tempt = prompt("Enter temprature");
+
+// if (tempt > 40) {
+//   alert("It is too hot outside.");
+// }
+// else if(tempt > 30){
+//   alert("The Weather today is Normal.")
+// }
+// else if(tempt > 20){
+//   alert("Today’s Weather is cool.")
+// }
+// else if(tempt > 10){
+//   alert("OMG! Today’s weather is so Cool.")
+// }
+// else {
+//   alert("Temprature is like freezing");
+// }
+
+
+// q11
+
+// var num = prompt("Enter your number");
+// var operator = prompt("enter operator");
+// var num2 = prompt("Enter second number");
+// if (operator === "+") {
+//   alert(num + num2);
+// }
+// else if(operator === "-"){
+//   alert(num - num2)
+// }
+// else if(operator === "*"){
+//   alert(num * num2)
+// }
+// else if(operator === "/"){
+//   alert(num / num2)
+// }
+// else if(operator === "%"){
+//   alert(num % num2)
+// }
+// else {
+//   alert("Nan");
+// }
+
+//chapter 11 & 23
+//q1
+// var char = window.prompt("Enter a character","");
+// var charCode = char.charCodeAt();
+// if(charCode>=65 && charCode <= 90)
+// {
+// alert("Capital");
+// }
+// else if(charCode >= 97 && charCode <= 122){
+// alert("Small")
+// }
+// else if(char < 0){
+//     alert("this is negative number")
+// }
+// else if(char === "0"){
+//     alert("This is Zero")
+// }
+// else if(char > 0){
+//     alert("this is positive number")
+// }    
+  
